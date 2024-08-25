ef2a8774bc559a0335083b1a2b084410bcef953a
XuanTruong1810 <truongtamcobra@gmail.com>
2024-08-25 18:59:27 +0700

    fix model

--- core/ModelBase.php
+++ core/ModelBase.php
@@ -2,14 +2,15 @@
 class ModelBase
 {
     private $serverName = "localhost";
+    private $port = "3306";
     private $username = "root";
-    private $password = "";
+    private $password = "root";
     private $database = "pnj_shop";
     public $conn;
     public function __construct()
     {
         try {
-            $this->conn = new PDO("mysql:host=$this->serverName;dbname=$this->database", $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=$this->serverName;port={$this->port};dbname=$this->database", $this->username, $this->password);
             $this->conn->exec("set names utf8mb4");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
